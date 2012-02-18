60e9fdc40a2a3d4fc256d835febfcac453da4541
apple <apple@applematoimac.(none)>
2012-02-18 12:41:59 +0800

    Initial Commit

---
+++ PageFlipDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PageFlipDemo
+//
+//  Created by  on 12-2-18.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
